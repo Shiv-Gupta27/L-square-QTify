703ac856d55e7596ae987ffdb17eb3e09ec95fe9
unknown <shivprasadgupta056@gmail.com>
2024-11-27 21:47:20 +0530

    navbar done

---
+++ qtify/src/Components/Logo/Logo.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import logoimage from "../../assets/LOGO.png"
+
+function Logo (){
+    return <img src={logoimage} alt="Qtify-logo" width={67} />
+}
+
+export default Logo;
---
+++ qtify/src/Pages/AlbumPage/AlbumPage.jsx
@@ -0,0 +1,5 @@
+function AlbumPage (){
+
+}
+
+export default AlbumPage;
